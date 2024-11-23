3d55197f9c1138194c8ca17960b00066225f2b7d
kust <vad.velikanov@yandex.ru>
2024-11-23 12:02:38 +0300

    raw login + register pages

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True  # Remove in final version
+app.jinja_env.auto_reload = True  # Remove in final version
+
+@app.route('/')
+def main():
+    return render_template("main.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
 if __name__ == '__main__':
-    pass  # Do something
+    app.run(host='localhost', port=5500, debug=True)
